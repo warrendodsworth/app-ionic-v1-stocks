28225ce26434e2ccd92b05485b5de55c37103347
warrendodsworth <warren@jobadder.com>
2016-09-26 14:14:12 +1000

    add portfolio tab

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,15 @@
 angular.module('App', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+  .config(function ($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/tabs/quotes');
+  })
